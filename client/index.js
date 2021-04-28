6f801c968a1b322ca4a17ea14d6eeb7505d2f368
AndreMoutas <47751865+AndreMoutas@users.noreply.github.com>
2021-04-28 12:46:31 +0100

    Implemented login and register commands on client
    WiP: Message sending is not working properly. Body is empty (undefined)
    
    Co-authored-by: @sergiobrunodias

--- client/index.js
+++ client/index.js
@@ -1,6 +1,10 @@
 const registration = require("./authentication");
 const axios = require("axios");
 
+// Create app
+const express = require("express");
+const app = express();
+
 // Adds Authorization header automatically
 axios.interceptors.request.use(
     request => {
@@ -38,12 +42,63 @@ async function RequestNrt() {
     console.log(result.data);
 }
 
+app.post("/message",async (req,res) => {
+    console.log(req.body);
+    //const { content } = req.body;
+
+    try {
+        //console.log("Content: " + content);
+        return res.status(200).json({ token: "ola"});
+    }
+    catch (err) {
+        console.error(err)
+        return res.status(401).json(err.message);
+    }
+})
+
+const port = process.argv[2];
+
+app.listen(port, async () => {
+    console.log(`Client running on ${process.env.NODE_ENV} mode, at port ${port}.`)
+})
+
+var stdin = process.openStdin();
+
+stdin.addListener("data", function(input) {
+    let inputArray = input.toString().trim().split(" ");
+    switch(inputArray[0]) {
+        case "register":
+            register(inputArray[1], inputArray[2], inputArray[3]);
+            break;
+        case "login":
+            login(inputArray[1]); 
+            break;
+        case "message":
+            sendMessage(inputArray[1]);
+            break;
+    }
+});
+
+function register(username, oneTimeID, password) {
+    registration.Register(username, oneTimeID, password);
+}
+function login(password) {
+    registration.Login(password);
+}
+
+async function sendMessage(receiver) {
+    const result = await axios.post("http://localhost:3001/message", {
+        text: "ping" 
+    });
+}
+
 //RequestSqrt();
 //RequestCbrt();
 // RequestNrt();
 
-/*
-registration.Register('William','k0CkalZpZRD9','epic');
-*/
-// registration.Register('William','obxV4VtyMl0F','epic9000');
-//registration.Login('epic9000');
+
+//registration.Register('William','obxV4VtyMl0F','epic9000');
+//registration.Register('JohnSmi','3Vl2lUGwB7Wm','epic9000');
+// registration.Login('epic9000');
+// William obxV4VtyMl0F epic9000
+// JohnSmi 3Vl2lUGwB7Wm epic9000
