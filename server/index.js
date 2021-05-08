8e5a0478a335fc63d343b48738f08be77abe70d4
Sérgio Dias <up201704889@fe.up.pt>
2021-05-08 11:24:24 +0100

    Fixed small issues
    Co-authored-by: @AndreMoutas

--- server/index.js
+++ server/index.js
@@ -11,12 +11,13 @@ const db = require("./database/index");
 
 // Body parser middleware
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const preRegistration = require("./services/pre-registration");
 const operations = require("./services/operations");
 const authentication = require("./services/authentication");
+const storage = require("./storage/Storage");
 
 /*
 // Setup session and passport
@@ -90,10 +91,24 @@ app.post("/register", async (req, res) => {
 })
 
 app.post("/login", async (req, res) => {
-    const { username, password } = req.body;
+    const { username, password, sender } = req.body;
     try {
         const token = await authentication.login(username, password, req, res); // !
+        storage.addClientInfo(username, "localhost", sender);
         return res.json({ token: token });
+    }
+    catch (err) {
+        console.error(err)
+        return res.status(401).json(err.message);
+    }
+})
+
+app.post("/clientInfo", authentication.authenticateMiddleware, async (req, res) => {
+    const { username } = req.body;
+    
+    try {
+        let clientInfo = storage.getClientInfo(username);
+        return res.status(200).json({ clientInfo: clientInfo });
     }
     catch (err) {
         console.error(err)
@@ -114,12 +129,10 @@ const port = process.env.APP_PORT || 3001;
 
 
 app.listen(port, async () => {
-    console.log(`App running on ${process.env.NODE_ENV} mode, at port ${port}.`)
+    console.log(`Server running on ${process.env.NODE_ENV} mode, at port ${port}.`)
     await db.connect();
     // await db.reset();
     // await store.sync();
-
-    // preRegistration.addUser("William Smith", 1)
 })
 
 var stdin = process.openStdin();
