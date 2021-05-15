1c77d45e562475614124d4e9f283dc24e7797131
Sérgio Dias <up201704889@fe.up.pt>
2021-05-15 11:57:17 +0100

    Added home page skeleton

--- client_interface/src/App.js
+++ client_interface/src/App.js
@@ -1,9 +1,10 @@
   
 import React, { Component } from "react";
-import { Button } from 'react-bootstrap';
+import { Button, Form, Row } from 'react-bootstrap';
 //import logo from "./logo.svg";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './images/logo.png';
 //import axios from 'axios';
 
 class App extends Component {
@@ -24,18 +25,67 @@ class App extends Component {
     }
 
     componentDidMount() {
+        document.body.style.backgroundColor = "black"
         this.callAPI();
     }
 
     render() {
         return (
-            <div className="App">
+            <div className="App" style={styles.div}>
+                <img src={logo} alt="Logo" />;
                 {/* <p className="App-intro">{this.state.apiResponse}</p> */}
-                <Button variant="outline-primary" size="lg" backgroundColor="blue">
-                    Secondary
-                </Button>
+                <Row style={styles.row}>
+                    <Form>
+                        <Form.Group controlId="number">
+                            <Form.Label>Number</Form.Label>
+                            <Form.Control type="email" placeholder="Enter number" />
+                            <Form.Text className="text-muted">
+                                Operand of the square root operation
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Button variant="dark" type="submit">
+                            Calculate
+                        </Button>
+                    </Form>
+                </Row>
+                <Row style={styles.row}>
+                    <Form>
+                        <Form.Group controlId="number">
+                            <Form.Label>Number</Form.Label>
+                            <Form.Control type="email" placeholder="Enter number" />
+                            <Form.Text className="text-muted">
+                                Operand of the square root operation
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Button variant="dark" type="submit">
+                            Calculate
+                        </Button>
+                    </Form>
+                </Row>
             </div>
         );
+    }
+}
+
+const styles = {
+    div: {
+        backgroundColor: 'black',
+    },
+    row: {
+        border: '1px solid rgba(0, 0, 0, 0.05)', 
+        borderRadius: '30px',
+        color: 'white',
+        backgroundColor: 'darkGray',
+        width: '50%',
+        margin: 'auto',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '10px',
+        marginBottom: '20px',
+        marginTop: '20px',
     }
 }
 
